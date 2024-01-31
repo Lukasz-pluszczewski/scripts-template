8a9547bccb072e46998b013db01bda96631f6c2b
lukasz <lukasz.pluszczewski@gmail.com>
2024-01-31 19:20:51 +0100

    Added start:watch script, updated async collections

--- src/utils/asyncCollections.ts
+++ src/utils/asyncCollections.ts
@@ -48,6 +48,27 @@ export async function asyncReduce<T, U>(array: T[], callback: (accumulator: U, v
   return accumulator as U;
 }
 
+export async function asyncFilter<T>(array: T[], callback: (value: T, index: number, array: T[]) => Promise<boolean | LastClass<boolean> | BreakSymbol>): Promise<T[]> {
+  const result: T[] = [];
+  await asyncForEach(array, async (value, index, array) => {
+    const iterationResult = await callback(value, index, array);
+    if (iterationResult instanceof LastClass) {
+      if (iterationResult.value) {
+        result.push(value);
+      }
+      return Break;
+    }
+    if (iterationResult === Break) {
+      return Break;
+    }
+    if (iterationResult) {
+      result.push(value);
+    }
+  });
+
+  return result;
+};
+
 // --- async sequential - object ---
 export async function asyncMapValues<T, U>(object: Record<ObjectKey, T>, callback: (value: T, key: string, object: Record<ObjectKey, T>) => Promise<U | BreakSymbol | LastClass<U>>): Promise<Record<string, U>> {
   const result: Record<string, U> = {};
@@ -68,31 +89,30 @@ export async function asyncMapValues<T, U>(object: Record<ObjectKey, T>, callbac
 }
 
 export async function asyncMapEntries<T, U>(object: Record<ObjectKey, T>, callback: (entry: ObjectEntry<T>, index: number, entries: ObjectEntry<T>[]) => Promise<U | BreakSymbol | LastClass<U>>): Promise<U[]> {
-  const entries = Object.entries(object);
-  return asyncMap(entries, callback);
+  return asyncMap(Object.entries(object), callback);
 }
 
 export async function asyncMapKeys<T, U>(object: Record<ObjectKey, T>, callback: (key: string, index: number, keys: string[]) => Promise<U | BreakSymbol | LastClass<U>>): Promise<U[]> {
-  const keys = Object.keys(object);
-  return asyncMap(keys, callback);
+  return asyncMap(Object.keys(object), callback);
 }
 
 export async function asyncForEachValues<T>(object: Record<ObjectKey, T>, callback: (value: T, key: string, object: Record<ObjectKey, T>) => Promise<void | BreakSymbol>): Promise<void> {
   for (const key in object) {
     if (object.hasOwnProperty(key)) {
-      await callback(object[key], key, object);
+      const iterationResult = await callback(object[key], key, object);
+      if (iterationResult === Break) {
+        break;
+      }
     }
   }
 }
 
 export async function asyncForEachEntries<T>(object: Record<ObjectKey, T>, callback: (entry: ObjectEntry<T>, index: number, entries: ObjectEntry<T>[]) => Promise<void | BreakSymbol>): Promise<void> {
-  const entries = Object.entries(object);
-  await asyncForEach(entries, callback);
+  await asyncForEach(Object.entries(object), callback);
 }
 
 export async function asyncForEachKeys<T>(object: Record<ObjectKey, T>, callback: (key: string, index: number, keys: string[]) => Promise<void | BreakSymbol>): Promise<void> {
-  const keys = Object.keys(object);
-  await asyncForEach(keys, callback);
+  await asyncForEach(Object.keys(object), callback);
 }
 
 export async function asyncReduceValues<T, U>(object: Record<ObjectKey, T>, callback: (accumulator: U, value: T, key: string, object: Record<ObjectKey, T>) => Promise<U | LastClass<U>>, initialValue?: U): Promise<U> {
@@ -111,13 +131,40 @@ export async function asyncReduceValues<T, U>(object: Record<ObjectKey, T>, call
 }
 
 export async function asyncReduceEntries<T, U>(object: Record<ObjectKey, T>, callback: (accumulator: U, entry: ObjectEntry<T>, index: number, entries: ObjectEntry<T>[]) => Promise<U | LastClass<U>>, initialValue?: U): Promise<U> {
-  const entries = Object.entries(object);
-  return asyncReduce(entries, callback, initialValue);
+  return asyncReduce(Object.entries(object), callback, initialValue);
 }
 
 export async function asyncReduceKeys<T, U>(object: Record<ObjectKey, T>, callback: (accumulator: U, key: string, index: number, keys: string[]) => Promise<U | LastClass<U>>, initialValue?: U): Promise<U> {
-  const keys = Object.keys(object);
-  return asyncReduce(keys, callback, initialValue);
+  return asyncReduce(Object.keys(object), callback, initialValue);
+}
+
+export async function asyncFilterValues<T>(object: Record<ObjectKey, T>, callback: (value: T, key: string, object: Record<ObjectKey, T>) => Promise<boolean | LastClass<boolean> | BreakSymbol>): Promise<Record<string, T>> {
+  const result: Record<string, T> = {};
+  await asyncForEachValues(object, async (value, key, object) => {
+    const iterationResult = await callback(value, key, object);
+    if (iterationResult instanceof LastClass) {
+      if (iterationResult.value) {
+        result[key] = value;
+      }
+      return Break;
+    }
+    if (iterationResult === Break) {
+      return Break;
+    }
+    if (iterationResult) {
+      result[key] = value;
+    }
+  });
+
+  return result;
+}
+
+export async function asyncFilterKeys<T>(object: Record<ObjectKey, T>, callback: (key: string, index: number, keys: string[]) => Promise<boolean | LastClass<boolean> | BreakSymbol>): Promise<string[]> {
+  return asyncFilter(Object.keys(object), callback);
+}
+
+export async function asyncFilterEntries<T>(object: Record<ObjectKey, T>, callback: (entry: ObjectEntry<T>, index: number, entries: ObjectEntry<T>[]) => Promise<boolean | LastClass<boolean> | BreakSymbol>): Promise<ObjectEntry<T>[]> {
+  return asyncFilter(Object.entries(object), callback);
 }
 
 
@@ -128,6 +175,11 @@ export async function asyncMapParallel<T, U>(array: T[], callback: (value: T, in
 
 export async function asyncForEachParallel<T>(array: T[], callback: (value: T, index: number, array: T[]) => Promise<void>): Promise<void> {
   await Promise.all(array.map(callback));
+}
+
+export async function asyncFilterParallel<T>(array: T[], callback: (value: T, index: number, array: T[]) => Promise<boolean>): Promise<T[]> {
+  const result = await Promise.all(array.map(callback));
+  return array.filter((_, index) => result[index]);
 }
 
 // --- async parallel - object ---
@@ -163,4 +215,34 @@ export async function asyncForEachEntriesParallel<T>(object: Record<ObjectKey, T
 export async function asyncForEachKeysParallel<T>(object: Record<ObjectKey, T>, callback: (key: string, index: number, keys: string[]) => Promise<void>): Promise<void> {
   const keys = Object.keys(object);
   await asyncForEachParallel(keys, callback);
+}
+
+export async function asyncFilterValuesParallel<T>(object: Record<ObjectKey, T>, callback: (value: T, key: string, object: Record<ObjectKey, T>) => Promise<boolean>): Promise<Record<string, T>> {
+  const result: Record<string, T> = {};
+  await Promise.all(Object.keys(object).map(async (key) => {
+    if (await callback(object[key], key, object)) {
+      result[key] = object[key];
+    }
+  }));
+  return result;
+}
+
+export async function asyncFilterKeysParallel<T>(object: Record<ObjectKey, T>, callback: (key: string, index: number, keys: string[]) => Promise<boolean>): Promise<string[]> {
+  const keys = Object.keys(object);
+  const result = await Promise.all(keys.map(async (key, index) => {
+    if (await callback(key, index, keys)) {
+      return key;
+    }
+  }));
+  return result.filter(Boolean) as string[];
+}
+
+export async function asyncFilterEntriesParallel<T>(object: Record<ObjectKey, T>, callback: (entry: ObjectEntry<T>, index: number, entries: ObjectEntry<T>[]) => Promise<boolean>): Promise<ObjectEntry<T>[]> {
+  const entries = Object.entries(object);
+  const result = await Promise.all(entries.map(async (entry, index) => {
+    if (await callback(entry, index, entries)) {
+      return entry;
+    }
+  }));
+  return result.filter(Boolean) as ObjectEntry<T>[];
 }
